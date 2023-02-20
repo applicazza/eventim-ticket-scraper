a7053c9342aea80286a5dc340401397a40f4aaec
Artyom Sokolov <artyom@applicazza.com>
2023-02-20 22:27:39 +0200

    Add scheduler

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { executablePath } from 'puppeteer';
 import mailer from '@sendgrid/mail';
 import { cleanEnv, str } from 'envalid';
 import { Logger } from 'tslog';
+import schedule from 'node-schedule';
+
+process.on('SIGINT', function () {
+  schedule.gracefulShutdown().then(() => process.exit(0));
+});
 
 const env = cleanEnv(process.env, {
   EVENTIM_URL: str(),
@@ -16,38 +21,40 @@ const logger = new Logger();
 
 mailer.setApiKey(env.SENDGRID_API_KEY);
 
-(async () => {
-  logger.info('Starting puppeteer');
+schedule
+  .scheduleJob('*/5 * * * *', async function () {
+    logger.info('Starting puppeteer');
 
-  const browser = await puppeteer.use(StealthPlugin()).launch({
-    headless: true,
-    executablePath: executablePath(),
-  });
-
-  const page = await browser.newPage();
-
-  logger.info(`Opening ${env.EVENTIM_URL}`);
-  await page.goto(env.EVENTIM_URL);
-
-  let fastBookingFirstElement = await page.$(
-    'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
-  );
-
-  if (fastBookingFirstElement) {
-    logger.info('Sending an email');
-
-    await mailer.send({
-      to: env.SENDGRID_TO.split(','),
-      from: env.SENDGRID_FROM,
-      subject: 'Tickets for sale detected',
-      text: `There are tickets to grab! Go to ${env.EVENTIM_URL}`,
-      html: `There are tickets to grab! <a href="${env.EVENTIM_URL}">Eventim</a>`,
+    const browser = await puppeteer.use(StealthPlugin()).launch({
+      headless: true,
+      executablePath: executablePath(),
     });
-  } else {
-    logger.info('Will try our luck later....');
-  }
 
-  logger.info('Closing puppeteer');
+    const page = await browser.newPage();
 
-  await browser.close();
-})();
+    logger.info(`Opening ${env.EVENTIM_URL}`);
+    await page.goto(env.EVENTIM_URL);
+
+    let fastBookingFirstElement = await page.$(
+      'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
+    );
+
+    if (fastBookingFirstElement) {
+      logger.info('Sending an email');
+
+      await mailer.send({
+        to: env.SENDGRID_TO.split(','),
+        from: env.SENDGRID_FROM,
+        subject: 'Tickets for sale detected',
+        text: `There are tickets to grab! Go to ${env.EVENTIM_URL}`,
+        html: `There are tickets to grab! <a href="${env.EVENTIM_URL}">Eventim</a>`,
+      });
+    } else {
+      logger.info('Will try our luck later....');
+    }
+
+    logger.info('Closing puppeteer');
+
+    await browser.close();
+  })
+  .invoke();
