d7891e81c0d91475c1f87553e993d4eae66db733
Artyom Sokolov <artyom@applicazza.com>
2023-02-20 22:14:32 +0200

    Use `envalid` to force environment variables to be set

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { executablePath } from 'puppeteer';
 import mailer from '@sendgrid/mail';
+import { cleanEnv, str } from 'envalid';
 
-if (!process.env.SENDGRID_API_KEY) {
-  process.exit(1);
-}
+const env = cleanEnv(process.env, {
+  EVENTIM_URL: str(),
+  SENDGRID_API_KEY: str(),
+  SENDGRID_FROM: str(),
+  SENDGRID_TO: str(),
+});
 
-mailer.setApiKey(process.env.SENDGRID_API_KEY);
+mailer.setApiKey(env.SENDGRID_API_KEY);
 
 (async () => {
   const browser = await puppeteer.use(StealthPlugin()).launch({
@@ -17,27 +21,19 @@ mailer.setApiKey(process.env.SENDGRID_API_KEY);
 
   const page = await browser.newPage();
 
-  if (!process.env.EVENTIM_URL) {
-    process.exit(1);
-  }
-
-  await page.goto(process.env.EVENTIM_URL);
+  await page.goto(env.EVENTIM_URL);
 
   let fastBookingFirstElement = await page.$(
     'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
   );
 
   if (fastBookingFirstElement) {
-    if (!process.env.SENDGRID_FROM || !process.env.SENDGRID_TO) {
-      process.exit(1);
-    }
-
     await mailer.send({
-      to: process.env.SENDGRID_TO.split(','),
-      from: process.env.SENDGRID_FROM,
+      to: env.SENDGRID_TO.split(','),
+      from: env.SENDGRID_FROM,
       subject: 'Tickets for sale detected',
-      text: `There are tickets to grab! Go to ${process.env.EVENTIM_URL}`,
-      html: `There are tickets to grab! <a href="${process.env.EVENTIM_URL}">Eventim</a>`,
+      text: `There are tickets to grab! Go to ${env.EVENTIM_URL}`,
+      html: `There are tickets to grab! <a href="${env.EVENTIM_URL}">Eventim</a>`,
     });
   }
 
