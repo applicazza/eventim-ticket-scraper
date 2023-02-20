8e196d3db3603baaaf99188f9bedee6722befba8
Artyom Sokolov <artyom@applicazza.com>
2023-02-20 22:11:31 +0200

    Send email when tickets for sale detected

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { executablePath } from 'puppeteer';
+import mailer from '@sendgrid/mail';
+
+if (!process.env.SENDGRID_API_KEY) {
+  process.exit(1);
+}
+
+mailer.setApiKey(process.env.SENDGRID_API_KEY);
 
 (async () => {
   const browser = await puppeteer.use(StealthPlugin()).launch({
@@ -11,31 +18,27 @@ import { executablePath } from 'puppeteer';
   const page = await browser.newPage();
 
   if (!process.env.EVENTIM_URL) {
-    return;
+    process.exit(1);
   }
 
   await page.goto(process.env.EVENTIM_URL);
 
-  let items = await page.$x(
-    '//div[contains(concat(" ", normalize-space(@class), " "), " ticket-type-item ")]'
+  let fastBookingFirstElement = await page.$(
+    'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
   );
 
-  for (const item of items) {
-    const titleContainer = await item.$('xpath=preceding-sibling::*');
-    const priceContainer = await item.$('.ticket-type-price');
-    const isUnavailableContainer = await item.$('.ticket-type-unavailable-sec');
+  if (fastBookingFirstElement) {
+    if (!process.env.SENDGRID_FROM || !process.env.SENDGRID_TO) {
+      process.exit(1);
+    }
 
-    const title = await titleContainer?.evaluate((el) =>
-      el.textContent?.trim()
-    );
-    const price = await priceContainer?.evaluate((el) =>
-      el.textContent?.trim()
-    );
-    const isUnavailable = await isUnavailableContainer?.evaluate((el) =>
-      el.textContent?.trim()
-    );
-
-    console.log(`${title} -> ${price} -> ${isUnavailable}`);
+    await mailer.send({
+      to: process.env.SENDGRID_TO.split(','),
+      from: process.env.SENDGRID_FROM,
+      subject: 'Tickets for sale detected',
+      text: `There are tickets to grab! Go to ${process.env.EVENTIM_URL}`,
+      html: `There are tickets to grab! <a href="${process.env.EVENTIM_URL}">Eventim</a>`,
+    });
   }
 
   await browser.close();
