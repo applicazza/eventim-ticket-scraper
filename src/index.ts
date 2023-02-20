abd0ea1dd621dd6cf683f172a80e6d6a093fcbd6
Artyom Sokolov <artyom@applicazza.com>
2023-02-20 22:39:25 +0200

    Add build and start scripts

--- src/index.ts
+++ src/index.ts
@@ -21,40 +21,39 @@ const logger = new Logger();
 
 mailer.setApiKey(env.SENDGRID_API_KEY);
 
-schedule
-  .scheduleJob('*/5 * * * *', async function () {
-    logger.info('Starting puppeteer');
+logger.info('Starting scraping every minute...');
+schedule.scheduleJob('* * * * *', async function () {
+  logger.info('Starting puppeteer');
 
-    const browser = await puppeteer.use(StealthPlugin()).launch({
-      headless: true,
-      executablePath: executablePath(),
+  const browser = await puppeteer.use(StealthPlugin()).launch({
+    headless: true,
+    executablePath: executablePath(),
+  });
+
+  const page = await browser.newPage();
+
+  logger.info(`Opening ${env.EVENTIM_URL}`);
+  await page.goto(env.EVENTIM_URL);
+
+  let fastBookingFirstElement = await page.$(
+    'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
+  );
+
+  if (fastBookingFirstElement) {
+    logger.info('Sending an email');
+
+    await mailer.send({
+      to: env.SENDGRID_TO.split(','),
+      from: env.SENDGRID_FROM,
+      subject: 'Tickets for sale detected',
+      text: `There are tickets to grab! Go to ${env.EVENTIM_URL}`,
+      html: `There are tickets to grab! <a href="${env.EVENTIM_URL}">Eventim</a>`,
     });
+  } else {
+    logger.info('Will try our luck later....');
+  }
 
-    const page = await browser.newPage();
+  logger.info('Closing puppeteer');
 
-    logger.info(`Opening ${env.EVENTIM_URL}`);
-    await page.goto(env.EVENTIM_URL);
-
-    let fastBookingFirstElement = await page.$(
-      'xpath=//div[contains(concat(" ", normalize-space(@class), " "), " pc-list-detail-fastbooking ")][1]'
-    );
-
-    if (fastBookingFirstElement) {
-      logger.info('Sending an email');
-
-      await mailer.send({
-        to: env.SENDGRID_TO.split(','),
-        from: env.SENDGRID_FROM,
-        subject: 'Tickets for sale detected',
-        text: `There are tickets to grab! Go to ${env.EVENTIM_URL}`,
-        html: `There are tickets to grab! <a href="${env.EVENTIM_URL}">Eventim</a>`,
-      });
-    } else {
-      logger.info('Will try our luck later....');
-    }
-
-    logger.info('Closing puppeteer');
-
-    await browser.close();
-  })
-  .invoke();
+  await browser.close();
+});
